8cd203e78a55f543ecb7037db9be35a52be6587d
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-04-16 09:05:04 +0600

    add router

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
-
+    render() {
+        return (
+        <div>
+            <h3>Home page</h3>
+        </div>
+        );
+    }
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+const application = (
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  application,
   document.getElementById('root')
 );
 
